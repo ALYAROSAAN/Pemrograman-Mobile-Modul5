73204d85d487dd56faab7473b6d09ebccda28377
ALYAROSAAN <145338548+ALYAROSAAN@users.noreply.github.com>
2025-06-12 01:38:06 +0800

    Update MyApplication.kt

--- COMPOSE/MyAnimeListApp/app/src/main/java/com/example/myanimelistapp/MyApplication.kt
+++ COMPOSE/MyAnimeListApp/app/src/main/java/com/example/myanimelistapp/MyApplication.kt
@@ -8,24 +8,21 @@ import com.example.myanimelistapp.repository.MovieRepository
 
 class MyApplication : Application() {
 
-    // Buat instance database dan repository di sini agar hanya ada satu di seluruh aplikasi
     lateinit var repository: MovieRepository
         private set
 
     override fun onCreate() {
         super.onCreate()
 
-        // Buat database
         val database = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "movie_database"
         ).build()
 
-        // Buat repository dengan semua dependency-nya
         repository = MovieRepository(
             apiService = RetrofitInstance.api,
             movieDao = database.movieDao(),
-            apiKey = "MASUKKAN_API_KEY_TMDB_ANDA_DI_SINI" // GANTI INI!
+            apiKey = "b073540aec31d15b415ff4876c8c0d37"
         )
     }
 }
